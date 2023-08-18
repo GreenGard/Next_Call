b9bbbe0d8bd79481f9b77be03e01bff9db558179
GreenGard <madelene.axblom@gmail.com>
2023-08-18 09:38:14 +0200

    add database

---
+++ dbo.Customers.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[Customers] ([Id], [Company], [Closing_date], [Software], [VAT_period]) VALUES (1, N'Test', N'2023-08-31', N'Visma', N'Månad')
+INSERT INTO [dbo].[Customers] ([Id], [Company], [Closing_date], [Software], [VAT_period]) VALUES (2, N'Testad2', N'2023-12-31', N'Fortnox', N'Kvartal')
